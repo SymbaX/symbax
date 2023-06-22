59a3b333bc36f456e61d2bc9fafb3b05b077c064
ENDOTAKUMI <endo.takumi.info@gmail.com>
2023-06-22 12:04:08 +0900

    newをcreateに変更

--- resources/views/event/new.blade.php
+++ resources/views/event/new.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('New event') }}
+            {{ __('Create event') }}
         </h2>
     </x-slot>
 
@@ -11,7 +11,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    @include('event.partials.new-event-form')
+                    @include('event.partials.create-event-form')
                 </div>
             </div>
         </div>
--- resources/views/event/partials/create-event-form.blade.php
+++ resources/views/event/partials/create-event-form.blade.php
@@ -1,7 +1,7 @@
 <section>
     <header>
         <h2 class="text-lg font-medium text-gray-900">
-            {{ __('New event') }}
+            {{ __('Create event') }}
         </h2>
 
         <p class="mt-1 text-sm text-gray-600">
