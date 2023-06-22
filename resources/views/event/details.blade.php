75962e32e8ef2945e1cf0446a4497f4ef1625547
ENDOTAKUMI <endo.takumi.info@gmail.com>
2023-06-22 14:04:34 +0900

    一応エラー処理を入れてみた

--- resources/views/event/details.blade.php
+++ resources/views/event/details.blade.php
@@ -9,15 +9,21 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
-                    <h1>{{ $event->name }}</h1>
-                    <p>{{ $event->details }}</p>
-                    <p>{{ $event->category }}</p>
-                    <p>{{ $event->tag }}</p>
-                    <p>{{ $event->conditions_of_participation }}</p>
-                    <p>{{ $event->extarnal_links }}</p>
-                    <p>{{ $event->datetime }}</p>
-                    <p>{{ $event->place }}</p>
-                    <p>{{ $event->number_of_people }}</p>
+                    @if ($event)
+                    <div class="max-w-xl">
+                        <h1>{{ $event->name }}</h1>
+                        <p>{{ $event->details }}</p>
+                        <p>{{ $event->category }}</p>
+                        <p>{{ $event->tag }}</p>
+                        <p>{{ $event->conditions_of_participation }}</p>
+                        <p>{{ $event->external_links }}</p>
+                        <p>{{ $event->datetime }}</p>
+                        <p>{{ $event->place }}</p>
+                        <p>{{ $event->number_of_people }}</p>
+                    </div>
+                    @else
+                    <p>Event not found.</p>
+                    @endif
                 </div>
             </div>
         </div>
