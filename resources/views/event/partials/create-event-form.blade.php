fd001e4951826c3f81d4a9a09e2393a710860380
ENDOTAKUMI <endo.takumi.info@gmail.com>
2023-06-27 16:19:08 +0900

    外部リンク以外のautocompleteをoff

--- resources/views/event/partials/create-event-form.blade.php
+++ resources/views/event/partials/create-event-form.blade.php
@@ -20,14 +20,14 @@
         <div>
             <x-input-label for="name" :value="__('Event Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', '')"
-                required autofocus autocomplete="name" />
+                required autofocus autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
             <x-input-label for="details" :value="__('Details')" />
             <x-textarea id="details" name="details" type="text" class="mt-1 block w-full"
-                 required autocomplete="details">
+                 required autocomplete="off">
                  {{ old('details','')}}
             </x-textarea>
             <x-input-error class="mt-2" :messages="$errors->get('details')" />
@@ -36,14 +36,14 @@
         <div>
             <x-input-label for="category" :value="__('Category')" />
             <x-text-input id="category" name="category" type="text" class="mt-1 block w-full" :value="old('category', '')"
-                required autocomplete="category" />
+                required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('category')" />
         </div>
 
         <div>
             <x-input-label for="tag" :value="__('Tag')" />
             <x-text-input id="tag" name="tag" type="text" class="mt-1 block w-full" :value="old('tag', '')"
-                required autocomplete="tag" />
+                required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('tag')" />
         </div>
 
@@ -51,14 +51,14 @@
             <x-input-label for="conditions_of_participation" :value="__('Conditions of participation')" />
             <x-text-input id="conditions_of_participation" name="conditions_of_participation" type="text"
                 class="mt-1 block w-full" :value="old('conditions_of_participation', '')" required
-                autocomplete="conditions_of_participation" />
+                autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('conditions_of_participation')" />
         </div>
 
         <div>
             <x-input-label for="extarnal_links" :value="__('Extarnal links')" />
             <x-text-input id="extarnal_links" name="extarnal_links" type="text" class="mt-1 block w-full"
-                :value="old('extarnal_links', '')" required autocomplete="extarnal_links" />
+                :value="old('extarnal_links', '')" required autocomplete="url" />
             <x-input-error class="mt-2" :messages="$errors->get('extarnal_links')" />
         </div>
 
@@ -66,7 +66,7 @@
         <div>
             <x-input-label for="datetime" :value="__('Datetime')" />
             <x-text-input id="datetime" name="datetime" type="text" class="mt-1 block w-full" :value="old('datetime', '')"
-                required autocomplete="datetime" />
+                required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('datetime')" />
         </div>
 
@@ -74,20 +74,20 @@
         <div>
             <x-input-label for="place" :value="__('Place')" />
             <x-text-input id="place" name="place" type="text" class="mt-1 block w-full" :value="old('place', '')"
-                required autocomplete="place" />
+                required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('place')" />
         </div>
 
         <div>
             <x-input-label for="number_of_people" :value="__('Number of people')" />
             <x-text-input id="number_of_people" name="number_of_people" type="text" class="mt-1 block w-full"
-                :value="old('number_of_people', '')" required autocomplete="number_of_people" />
+                :value="old('number_of_people', '')" required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('number_of_people')" />
         </div>
 
         <div>
             <x-input-label for="product_image" :value="__('product_image')" />
-            <x-text-input id="product_image" name="product_image" type="file" class="mt-1 block w-full" :value="old('product_image', '')" required autocomplete="product_image" />
+            <x-text-input id="product_image" name="product_image" type="file" class="mt-1 block w-full" :value="old('product_image', '')" required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('product_image')" />
         </div>
 
