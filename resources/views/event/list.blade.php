ee5bbbd16b57c2ca4b2a8969457a64dee2274827
ENDO Takumi <endo.takumi.info@gmail.com>
2023-06-29 15:39:49 +0900

    修正

--- resources/views/event/list.blade.php
+++ resources/views/event/list.blade.php
@@ -10,30 +10,32 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                <div class="max-w-xl">
-                    @if ($events->isEmpty())
+                @if ($events->isEmpty())
                     <p>No events found.</p>
-                    @else
-                    <div id="cardlayout-wrap"><!--カードレイアウトをラッピング -->
+                @else
+                    <div id="cardlayout-wrap">
+                        <!--カードレイアウトをラッピング -->
                         @foreach ($events as $event)
-                        <section class="card-list">
-                            <a class="card-link" href="#">
-                                <ul>
+                            <section class="card-list">
+                                <a class="card-link" href="#">
+                                    <ul>
 
-                                    <li><a href="details/{{ $event->id }}">
-                                            <class="card-title">{{ $event->name }}</class>
-                                                <figure class="card-figure"><img class="product_image mx-auto" src="{{ Storage::url($event->product_image) }}" alt=""></figure>
-                                        </a>
-                                    </li>
+                                        <li><a href="details/{{ $event->id }}">
+                                                <class="card-title">{{ $event->name }}</class>
+                                                    <figure class="card-figure"><img class="product_image mx-auto"
+                                                            src="{{ Storage::url($event->product_image) }}"
+                                                            alt=""></figure>
+                                            </a>
+                                        </li>
 
-                                </ul>
+                                    </ul>
 
-                            </a>
-                        </section>
+                                </a>
+                            </section>
                         @endforeach
-                    </div><!--カードレイアウトをラッピング -->
-                    @endif
-                </div>
+                    </div>
+                    <!--カードレイアウトをラッピング -->
+                @endif
                 <br />
                 {{ $events->links() }}
             </div>
