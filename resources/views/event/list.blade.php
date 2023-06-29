f6d4a390a2ea90af7795ea9aad7e267f8296e599
daiwasan <t.daiwa0219@gmail.com>
2023-06-29 16:04:15 +0900

    タイトル名中央ぞろえ成功

--- resources/views/event/list.blade.php
+++ resources/views/event/list.blade.php
@@ -11,30 +11,28 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @if ($events->isEmpty())
-                    <p>No events found.</p>
+                <p>No events found.</p>
                 @else
-                    <div id="cardlayout-wrap">
-                        <!--カードレイアウトをラッピング -->
-                        @foreach ($events as $event)
-                            <section class="card-list">
-                                <a class="card-link" href="#">
-                                    <ul>
-
-                                        <li><a href="details/{{ $event->id }}">
-                                                <class="card-title">{{ $event->name }}</class>
-                                                    <figure class="card-figure"><img class="product_image mx-auto"
-                                                            src="{{ Storage::url($event->product_image) }}"
-                                                            alt=""></figure>
-                                            </a>
-                                        </li>
-
-                                    </ul>
-
-                                </a>
-                            </section>
-                        @endforeach
-                    </div>
+                <div id="cardlayout-wrap">
                     <!--カードレイアウトをラッピング -->
+                    @foreach ($events as $event)
+                    <section class="card-list">
+                        <a class="card-link" href="#">
+                            <ul>
+
+                                <li><a href="details/{{ $event->id }}">
+                                        <h3 class="card-title">{{ $event->name }}</h3>
+                                        <figure class="card-figure"><img class="product_image mx-auto" src="{{ Storage::url($event->product_image) }}" alt=""></figure>
+                                    </a>
+                                </li>
+
+                            </ul>
+
+                        </a>
+                    </section>
+                    @endforeach
+                </div>
+                <!--カードレイアウトをラッピング -->
                 @endif
                 <br />
                 {{ $events->links() }}
