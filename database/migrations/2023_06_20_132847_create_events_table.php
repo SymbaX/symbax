88988f0382b45001e9d4eb65f48951bd15df5d14
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-08 11:09:37 +0900

    DBの項目を変更

--- database/migrations/2023_06_20_132847_create_events_table.php
+++ database/migrations/2023_06_20_132847_create_events_table.php
@@ -12,19 +12,20 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('events', function (Blueprint $table) {
-            $table->increments('id');
+            $table->id();
             $table->string('name');
-            $table->string('details', 1000);
+            $table->string('detail', 1000);
             $table->string('category');
             $table->string('tag');
-            $table->string('conditions_of_participation');
-            $table->string('extarnal_links');
-            $table->datetime('datetime');
+            $table->string('participation_condition');
+            $table->string('external_Link');
+            $table->date('date');
+            $table->date('deadline_date');
             $table->string('place');
             $table->integer('number_of_people');
-            $table->string('product_image');
-            $table->unsignedBigInteger('creator_id');
-            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
+            $table->string('image_path');
+            $table->unsignedBigInteger('organizer_id');
+            $table->foreign('organizer_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
--- database/migrations/2023_06_28_144920_create_event_participants_table.php
+++ database/migrations/2023_06_28_144920_create_event_participants_table.php
@@ -16,7 +16,7 @@ class CreateEventParticipantsTable extends Migration
         Schema::create('event_participants', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id');
-            $table->unsignedInteger('event_id');
+            $table->unsignedBigInteger('event_id');
             $table->timestamps();
 
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
