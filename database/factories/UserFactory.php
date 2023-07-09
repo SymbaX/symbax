7f176d460912b7cc63f764b999b3d01a7c1eeb84
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-09 11:24:55 +0900

    UserFactoryを残しておくようにした

---
+++ database/factories/UserFactory.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+/**
+ * ユーザーファクトリクラス
+ *
+ * ユーザーモデルのファクトリクラスです。`Factory`クラスを拡張し、`User`モデルのデフォルトの状態やメソッドを定義します。
+ */
+class UserFactory extends Factory
+{
+    /**
+     * モデルのデフォルトの状態を定義します。
+     *
+     * @return array<string,mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ];
+    }
+
+    /**
+     * モデルのメールアドレスが未確認であることを示します。
+     *
+     * @return $this
+     */
+    public function unverified(): static
+    {
+        return $this->state(fn (array $attributes) => [
+            'email_verified_at' => null,
+        ]);
+    }
+}
