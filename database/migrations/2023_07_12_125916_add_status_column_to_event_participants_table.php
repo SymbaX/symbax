fe17f35df5cb102198b4b80af86519f22cdc42be
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-12 13:01:42 +0900

    statusを追加した

---
+++ database/migrations/2023_07_12_125916_add_status_column_to_event_participants_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('event_participants', function (Blueprint $table) {
+            //
+            $table->string('status')->after('event_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('event_participants', function (Blueprint $table) {
+            //
+            $table->dropColumn('status');
+        });
+    }
+};
