d3a2f7a317fb893b3bd7b2e2da14be3c16660f63
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-15 01:36:19 +0900

    Validationを分割した

--- app/Http/Controllers/Event/CreateController.php
+++ app/Http/Controllers/Event/CreateController.php
@@ -3,30 +3,14 @@
 namespace App\Http\Controllers\Event;
 
 use App\Http\Controllers\Controller;
-use App\Http\Controllers\OperationLogController;
+use App\Http\Requests\Event\EventRequest;
 use App\Models\Event;
+use App\Models\OperationLog;
 use Illuminate\Contracts\View\View;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class CreateController extends Controller
 {
-    /**
-     * @var OperationLogController
-     */
-    private $operationLogController;
-
-    /**
-     * OperationLogControllerの新しいインスタンスを作成します。
-     *
-     * @param  OperationLogController  $operationLogController
-     * @return void
-     */
-    public function __construct(OperationLogController $operationLogController)
-    {
-        $this->operationLogController = $operationLogController;
-    }
-
     /**
      * イベント作成フォームの表示
      *
@@ -39,38 +23,27 @@ class CreateController extends Controller
         return view('event.create');
     }
 
-
     /**
      * イベントの作成
      *
      * リクエストから受け取ったデータを検証し、新しいイベントを作成します。
      *
-     * @param  Request  $request
+     * @param  EventRequest  $request
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function create(Request $request)
+    public function create(EventRequest $request)
     {
-        $validatedData = $request->validate([
-            'name' => ['required', 'max:20'],
-            'detail' => ['required', 'max:1000'],
-            'category' => ['required', 'max:30'],
-            'tag' => ['required', 'max:30'],
-            'participation_condition' => ['required', 'max:100'],
-            'external_link' => ['required', 'max:255', 'url'],
-            'date' => ['required', 'date'],
-            'deadline_date' => ['required', 'date'],
-            'place' => ['required', 'max:50'],
-            'number_of_recruits' => ['required', 'integer', 'min:1'],
-            'image_path' => ['required', 'max:5000', 'mimes:jpg,jpeg,png,gif'],
-        ]);
+        $validatedData = $request->validated();
 
         $validatedData['image_path'] = $request->file('image_path')->store('public/events');
-
         $validatedData['organizer_id'] = Auth::id();
 
         $event = Event::create($validatedData);
 
-        $this->operationLogController->store('イベントを作成しました ID:' . $event->id);
+        OperationLog::create([
+            'user_id' => $validatedData['organizer_id'],
+            'action' => 'イベントを作成しました ID:' . $event->id,
+        ]);
 
         return redirect()->back()->with('status', 'event-create');
     }
