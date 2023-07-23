260d4221b1f20d2d319800dc9d4726b16a9e3a8a
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-23 22:32:26 +0900

    コンフリクト修正ミスってたため修正

--- app/Http/Requests/Profile/ProfileUpdateRequest.php
+++ app/Http/Requests/Profile/ProfileUpdateRequest.php
@@ -21,7 +21,7 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['string', 'max:255'],
+            'name' => ['required', 'string', 'max:20'],
             'email' => ['email', 'max:255', Rule::unique(User::class)->ignore($this->user()->id)],
             'picture' => ['file', 'mimes:gif,png,jpg,webp', 'max:3072'],
         ];
