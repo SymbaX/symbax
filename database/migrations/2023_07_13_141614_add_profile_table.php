53ba4f2aac4265ce1ff9ccf4e8ddb1df905926c0
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-23 22:24:18 +0900

    カラムの追加位置とdownを追記

--- database/migrations/2023_07_13_141614_add_profile_table.php
+++ database/migrations/2023_07_13_141614_add_profile_table.php
@@ -12,8 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('self_introduction');
-            $table->string('profile_photo_path')->nullable();
+            $table->string('self_introduction')->nullable()->after('department_id');
+            $table->string('profile_photo_path')->nullable()->after('self_introduction');;
         });
     }
 
@@ -22,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('self_introduction');
+            $table->dropColumn('profile_photo_path');
+        });
     }
 };
