fb85652b829bb61b37f7f4138c75be98b6a4e41b
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-25 04:59:36 +0900

    縦幅を広げた

--- resources/views/components/textarea.blade.php
+++ resources/views/components/textarea.blade.php
@@ -2,6 +2,7 @@
 
 <textarea {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
     'class' => 'border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm',
+    'style' => 'height: 350px;',
 ]) !!}>
 {{ $value ?? $slot }}
 </textarea>
