ce6c56e36a93062dec32a687edcbdd19d495d904
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-24 13:22:14 +0900

    修正

--- database/migrations/2023_07_24_132018_add_userid_to_users_table.php
+++ database/migrations/2023_07_24_132018_add_userid_to_users_table.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -13,14 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('login_id')->after('id');
-        });
-
-        // Set initial unique values for the login_id
-        DB::table('users')->orderBy('id')->each(function ($user, $index) {
-            DB::table('users')
-                ->where('id', $user->id)
-                ->update(['login_id' => 'login_' . $index]);
+            $table->string('login_id')->unique()->change();
         });
     }
 
@@ -30,7 +22,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->dropColumn('login_id');
+            $table->dropUnique('login_id');
         });
     }
 };
