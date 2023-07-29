7bf91a54af3518d6bff587bd6c2b726c44727d90
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-30 00:23:12 +0900

    横並びの欄を作成

--- resources/views/event/list-upcoming.blade.php
+++ resources/views/event/list-upcoming.blade.php
@@ -1,7 +1,6 @@
 @push('css')
     <link rel="stylesheet" href="{{ asset('css/list.css') }}">
 @endpush
-
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
@@ -14,6 +13,25 @@
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 @if ($events->currentPage() === 1)
                     <h3>注目のイベント</h3>
+
+                    <div id="highlighted-event-wrap">
+                        @foreach ($events as $event)
+                            <section class="highlighted-event-card">
+                                <a class="card-link" href="#">
+                                    <ul>
+                                        <li><a href="{{ route('event.show', $event->id) }}">
+                                                <h3 class="highlighted-event-title">{{ $event->name }}</h3>
+                                                <figure class="card-figure"><img class="event_image mx-auto"
+                                                        src="{{ Storage::url($event->image_path) }}" alt="">
+                                                </figure>
+                                                <p class="highlighted-event-text">{{ $event->detail }}</p>
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </a>
+                            </section>
+                        @endforeach
+                    </div>
                 @endif
 
                 @include('event.partials.list')
