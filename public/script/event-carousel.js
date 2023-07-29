e02de53a45cf242db62b6ac68935380c1e24f45c
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-30 01:47:00 +0900

    scrollの量を調整

--- public/script/event-carousel.js
+++ public/script/event-carousel.js
@@ -1,31 +1,42 @@
 $(document).ready(function () {
     var scrollAmount = 0;
+    var cardWidth = $(".highlighted-event-card").outerWidth(true);
+
+    $(window).resize(function () {
+        var previousCardWidth = cardWidth;
+        cardWidth = $(".highlighted-event-card").outerWidth(true);
+        var currentCardIndex = Math.round(scrollAmount / previousCardWidth);
+        scrollAmount = currentCardIndex * cardWidth;
+        $("#highlighted-event-wrap").scrollLeft(scrollAmount);
+    });
 
     $("#carousel-button-prev").click(function () {
         if (scrollAmount > 0) {
-            scrollAmount -= 200;
+            scrollAmount -= cardWidth;
+            $("#highlighted-event-wrap").animate(
+                {
+                    scrollLeft: scrollAmount,
+                },
+                500
+            );
         }
-        $("#highlighted-event-wrap").animate(
-            {
-                scrollLeft: scrollAmount,
-            },
-            500
-        );
     });
 
     $("#carousel-button-next").click(function () {
-        if (
-            scrollAmount <
+        var maxScroll =
             $("#highlighted-event-wrap")[0].scrollWidth -
-                $("#highlighted-event-wrap").width()
-        ) {
-            scrollAmount += 200;
+            $("#highlighted-event-wrap").width();
+        if (scrollAmount < maxScroll) {
+            scrollAmount += cardWidth;
+            if (scrollAmount > maxScroll) {
+                scrollAmount = maxScroll;
+            }
+            $("#highlighted-event-wrap").animate(
+                {
+                    scrollLeft: scrollAmount,
+                },
+                500
+            );
         }
-        $("#highlighted-event-wrap").animate(
-            {
-                scrollLeft: scrollAmount,
-            },
-            500
-        );
     });
 });
