35717ae03d822e4933069f836e3ac21ec925221d
ENDO Takumi <endo.takumi.info@gmail.com>
2023-07-30 00:18:21 +0900

    upcomingの1ページ目のみ表示する欄を作成

--- resources/views/event/list-upcoming.blade.php
+++ resources/views/event/list-upcoming.blade.php
@@ -12,6 +12,10 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                @if ($events->currentPage() === 1)
+                    <h3>注目のイベント</h3>
+                @endif
+
                 @include('event.partials.list')
             </div>
         </div>
