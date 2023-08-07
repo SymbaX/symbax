9a5fe4f65c5026fc41e76b603039043e82c2b919
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-07 18:26:17 +0900

    add: Docを追記

--- public/script/community-reaction.js
+++ public/script/community-reaction.js
@@ -1,26 +1,32 @@
 $(document).ready(function () {
+    // ".py-12" クラスを持つ要素の "data-emojis" 属性から JSON を取得して解析します
     const emojis = JSON.parse($(".py-12").attr("data-emojis"));
 
+    // ".emoji-picker-button" クラスを持つ各要素に対して、クリックイベントリスナーを追加します
     $(".emoji-picker-button").click(function () {
         toggleEmojiPicker(this);
     });
 
+    // 引数として与えられた button 要素の隣にある要素の表示/非表示を切り替えます
     window.toggleEmojiPicker = function (button) {
         const $picker = $(button).next();
         $picker.toggle();
     };
 
+    // この関数は、"more" ボタンがクリックされたときに、その隣にある要素の表示/非表示を切り替えます
     window.toggleMoreEmojis = function (button) {
         const $moreEmojis = $(button).next();
         $moreEmojis.toggle();
     };
 
+    // ".emoji-tab-button" クラスを持つ各要素に対して、クリックイベントリスナーを追加します
     $(".emoji-tab-button").click(function () {
         const tabName = $(this).attr("data-tab");
         const $picker = $(this).parent().parent().parent();
         switchEmojiTab(tabName, $picker);
     });
 
+    // 指定されたタブをアクティブにし、そのタブの絵文字リストを表示します
     window.switchEmojiTab = function (tabName, $picker) {
         const $tabs = $picker.find(".emoji-tab-container .emoji-tabs button");
         $tabs.removeClass("active");
@@ -34,6 +40,7 @@ $(document).ready(function () {
 
         const emojiCategory = emojis[tabName];
 
+        // 取得した絵文字の各要素に対して以下の操作を行います
         emojiCategory.forEach((emoji) => {
             const $button = $("<button></button>");
             $button.attr("type", "button");
@@ -55,6 +62,7 @@ $(document).ready(function () {
         });
     };
 
+    // ページの読み込みが完了したときに以下の操作を行います
     $(window).on("load", function () {
         const $pickers = $(".emoji-picker");
         $pickers.each(function () {
