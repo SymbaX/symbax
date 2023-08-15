ebc6aa70771b3a0cb1541475f9dc21b93d519079
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-15 21:12:52 +0900

    fix: 必須、任意ラベルを多言語化

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -11,6 +11,8 @@
     <script type="text/javascript" src="{{ asset('script/jquery-3.7.0.min.js') }}"></script>
     <link rel="stylesheet" href="{{ asset('css/main.css') }}">
 
+    @include('style.main')
+
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
