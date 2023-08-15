d870e44e08fa51b615914667b811ee8959acb9f8
ENDO Takumi <endo.takumi.info@gmail.com>
2023-08-15 20:42:14 +0900

    fix: イベント編集ページに必須マークを追加

--- resources/views/event/partials/edit-event-form.blade.php
+++ resources/views/event/partials/edit-event-form.blade.php
@@ -16,21 +16,21 @@
 
         <input type="hidden" name="edit_token" value="{{ session('edit_token') }}">
         <div>
-            <x-input-label for="name" :value="__('Event Name')" />
+            <x-input-label for="name" class="required" :value="__('Event Name')" />
             <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $event->name)"
                 required autofocus autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('name')" />
         </div>
 
         <div>
-            <x-input-label for="detail" :value="__('Detail')" />
+            <x-input-label for="detail" class="required" :value="__('Detail')" />
             <x-textarea id="detail" name="detail" type="text" class="mt-1 block w-full" required
                 autocomplete="off">{{ old('detail', $event->detail) }}</x-textarea>
             <x-input-error class="mt-2" :messages="$errors->get('detail')" />
         </div>
 
         <div>
-            <x-input-label for="category" :value="__('Category')" />
+            <x-input-label for="category" class="required" :value="__('Category')" />
 
             <select name="category" id="category"
                 class="block mt-1 w-full border-gray-300 focus:border-indigo-500 focus:ring-indigo-500 rounded-md shadow-sm">
@@ -44,21 +44,21 @@
         </div>
 
         <div>
-            <x-input-label for="tag" :value="__('Tag')" />
+            <x-input-label for="tag" class="required" :value="__('Tag')" />
             <x-text-input id="tag" name="tag" type="text" class="mt-1 block w-full" :value="old('tag', $event->tag)"
                 required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('tag')" />
         </div>
 
         <div>
-            <x-input-label for="participation_condition" :value="__('Participation condition')" />
+            <x-input-label for="participation_condition" class="required" :value="__('Participation condition')" />
             <x-text-input id="participation_condition" name="participation_condition" type="text"
                 class="mt-1 block w-full" :value="old('participation_condition', $event->participation_condition)" required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('participation_condition')" />
         </div>
 
         <div>
-            <x-input-label for="external_link" :value="__('External link')" />
+            <x-input-label for="external_link" class="required" :value="__('External link')" />
             <x-text-input id="external_link" name="external_link" type="text" class="mt-1 block w-full"
                 :value="old('external_link', $event->external_link)" required autocomplete="url" />
             <x-input-error class="mt-2" :messages="$errors->get('external_link')" />
@@ -75,14 +75,14 @@
         </div>
 
         <div>
-            <x-input-label for="place" :value="__('Location')" />
+            <x-input-label for="place" class="required" :value="__('Location')" />
             <x-text-input id="place" name="place" type="text" class="mt-1 block w-full" :value="old('place', $event->place)"
                 required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('place')" />
         </div>
 
         <div>
-            <x-input-label for="number_of_recruits" :value="__('Number of recruits')" />
+            <x-input-label for="number_of_recruits" class="required" :value="__('Number of recruits')" />
             <x-text-input id="number_of_recruits" name="number_of_recruits" type="number" class="mt-1 block w-full"
                 :value="old('number_of_recruits', $event->number_of_recruits)" required autocomplete="off" />
             <x-input-error class="mt-2" :messages="$errors->get('number_of_recruits')" />
