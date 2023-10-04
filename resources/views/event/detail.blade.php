21adb9c87ea6d0b44adf2edd0e35e203114c11a5
MasayaSENGA <mail@sengamasaya.net>
2023-10-04 14:33:08 +0900

    スライダー廃止、サムネイルクリックするとでかくなる方式にした。

--- resources/views/event/detail.blade.php
+++ resources/views/event/detail.blade.php
@@ -18,78 +18,13 @@
                 <div class="event-detail">
                     @if ($event)
                         <div class="event-flex">
-                            <div class="carousel">
-                            <!-- スライドのリスト -->
-                            <div class="contains">
-                                <!-- スライドを選択するためのラジオボタンリスト。 -->
-                                <!-- 数は増減しても構わないです。 最初は1番目を選択状態(checked)にします。-->
-                                <!-- Slide各ラジオボタンに個別のidを定義して、nameはすべて同じ値にします。 -->
-                                <input class="slide_select" type="radio" id="SlideA" name="slide_check" checked />
-                                <input class="slide_select" type="radio" id="SlideB" name="slide_check" />
-                                <input class="slide_select" type="radio" id="SlideC" name="slide_check" />
-                                <input class="slide_select" type="radio" id="SlideD" name="slide_check" />
-                                <input class="slide_select" type="radio" id="SlideE" name="slide_check" />
-                                <!-- スライド -->
-                                <!-- 上のラジオボックスと同じ数だけ記述します。-->
-                                <div class="slide">
-                                <!-- スライドの前へ、次へとスクロールさせるボタン -->
-                                <div class="scroll_controler">
-                                    <!-- 前へボタン forで戻る先のラジオボタンのidを書きます。-->
-                                    <!-- 先頭要素なので、最後のスライドのidである"SlideE"を書いています。 -->
-                                    <label class="scroll_button scroll_prev" for="SlideE"></label>
-                                    <!-- 次へボタン 上と同様にforで進む先のラジオボタンのidを書きます。-->
-                                    <!-- 進み先は2番目の要素なので、2番目のスライドのid"SlideB"を書いています。 -->
-                                    <label class="scroll_button scroll_next" for="SlideB"></label>
-                                </div>
-                                <!-- スライドの内容（ここでは画像）を記述します。 -->
-                                <!-- div要素に変えれば文字を加えることもできます。 -->
-                                <img src="{{ Storage::url($event->image_path_a) }}">
-                                </div>
-                                <!-- スライド（2番目）内容は1個めと同じ -->
-                                <div class="slide">
-                                <div class="controler_scroll">
-                                    <label class="scroll_button scroll_prev" for="SlideA"></label>
-                                    <label class="scroll_button scroll_next" for="SlideC"></label>
-                                </div>
-                                <img src="{{ Storage::url($event->image_path_b) }}">
-                                </div>
-                                <!-- スライド（3番目）内容は1個めと同じ -->
-                                <div class="slide">
-                                <div class="controler_scroll">
-                                    <label class="scroll_button scroll_prev" for="SlideB"></label>
-                                    <label class="scroll_button scroll_next" for="SlideD"></label>
-                                </div>
-                                <img src="{{ Storage::url($event->image_path_c) }}">
-                                </div>
-                                <!-- スライド（4番目）内容は1個めと同じ -->
-                                <div class="slide">
-                                <div class="controler_scroll">
-                                    <label class="scroll_button scroll_prev" for="SlideC"></label>
-                                    <label class="scroll_button scroll_next" for="SlideE"></label>
-                                </div>
-                                <img src="{{ Storage::url($event->image_path_d) }}">
-                                </div>
-                                <!-- スライド（5番目）内容は1個めと同じ -->
-                                <div class="slide">
-                                <div class="controler_scroll">
-                                    <label class="scroll_button scroll_prev" for="SlideD"></label>
-                                    <label class="scroll_button scroll_next" for="SlideA"></label>
-                                </div>
-                                <img src="{{ Storage::url($event->image_path_e) }}">
-                                </div>
-                                <!-- スライド移動用ボタン -->
-                                <div class="move_controler">
-                                <!-- 1個目のスライドのボタン -->
-                                <!-- 一番上のラジオボタンの1個目のスライドのid”A”をforに定義します-->
-                                <label class="button_move" for="SlideA"></label>
-                                <label class="button_move" for="SlideB"></label>
-                                <label class="button_move" for="SlideC"></label>
-                                <label class="button_move" for="SlideD"></label>
-                                <label class="button_move" for="SlideE"></label>
-                                </div>
+                            <div class="thumbnail">
+                                <a href="{{ Storage::url($event->image_path_a) }}" data-lightbox="group"><img src="{{ Storage::url($event->image_path_a) }}" width="400"></a>
+                                <a href="{{ Storage::url($event->image_path_b) }}" data-lightbox="group"><img src="{{ Storage::url($event->image_path_b) }}" width="400"></a>
+                                <a href="{{ Storage::url($event->image_path_c) }}" data-lightbox="group"><img src="{{ Storage::url($event->image_path_c) }}" width="400"></a>
+                                <a href="{{ Storage::url($event->image_path_d) }}" data-lightbox="group"><img src="{{ Storage::url($event->image_path_d) }}" width="400"></a>
+                                <a href="{{ Storage::url($event->image_path_e) }}" data-lightbox="group"><img src="{{ Storage::url($event->image_path_e) }}" width="400"></a>
                             </div>
-                            </div>
-
                             <div class="right">
                                 <div class="scroll">
                                     <table border="1" cellspacing="0" cellpadding="5">
@@ -273,3 +208,7 @@
         </div>
     </div>
 </x-app-layout>
+
+<link href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/css/lightbox.css" rel="stylesheet">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.7.1/js/lightbox.min.js" type="text/javascript"></script>
