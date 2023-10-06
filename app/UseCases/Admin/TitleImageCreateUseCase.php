965b00336380113deebc63fcf1e429f39ad4b1a5
ENDO Takumi <endo.takumi.info@gmail.com>
2023-10-06 15:14:09 +0900

    feature: 一括生成処理を作成

--- app/UseCases/Admin/TitleImageCreateUseCase.php
+++ app/UseCases/Admin/TitleImageCreateUseCase.php
@@ -2,7 +2,9 @@
 
 namespace App\UseCases\Admin;
 
+use App\Models\Event;
 use App\UseCases\OperationLog\OperationLogUseCase;
+use Intervention\Image\Facades\Image;
 
 /**
  * タイトルイメージ生成のユースケースクラス
@@ -40,6 +42,39 @@ class TitleImageCreateUseCase
      */
     public function createImage()
     {
+        // イベントを取得する
+        $events =  Event::all();
+
+        foreach ($events as $event) {
+            $this->createEventOGP($event);
+        }
+
         return;
+    }
+
+    /**
+     * イベントのOGPを作成する
+     *
+     * @param Event $event
+     * @return void
+     */
+    private function createEventOGP(Event $event)
+    {
+        // OGPを生成
+        $path = public_path('img/base.png');
+        $img = Image::make($path);
+
+        // 画像にテキストを入れる。
+        $img->text(preg_replace('/[\x{10000}-\x{10FFFF}]/u', '', $event->name), 60, 220, function ($font) {
+            $font->file(public_path('fonts/NotoSansJP-SemiBold.ttf'));
+            $font->size(54);
+            $font->color("#000");
+            $font->align("left");
+            $font->valign("top");
+        });
+
+        // OGP画像を保存
+        $save_path = storage_path('app/public/event-titles/ogp_' . $event->id . '.png');
+        $img->save($save_path);
     }
 }
