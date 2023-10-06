ef75dc0dfb14df4d44e4766bd7c04b0709cc28ff
ENDO Takumi <endo.takumi.info@gmail.com>
2023-10-06 10:13:15 +0900

    fix: フォントをgitで管理し、パスを修正

--- app/UseCases/Event/EventEditUseCase.php
+++ app/UseCases/Event/EventEditUseCase.php
@@ -122,7 +122,7 @@ class EventEditUseCase
 
         // 画像にテキストを入れる。
         $img->text(preg_replace('/[\x{10000}-\x{10FFFF}]/u', '', $validatedData['name']), 60, 220, function ($font) { // 絵文字を除去する
-            $font->file(storage_path('app/public/fonts/NotoSansJP-SemiBold.ttf'));
+            $font->file(public_path('fonts/NotoSansJP-SemiBold.ttf'));
             $font->size(54);
             $font->color("#FFF");
             $font->align("left");
