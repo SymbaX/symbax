013d340ecdb8e3591d1c29113dade732d6adfa8f
MACHIDAMinoru <k020c1088@g.neec.ac.jp>
2023-10-10 14:19:52 +0900

    ページネーションを追加

--- resources/views/event/list-search.blade.php
+++ resources/views/event/list-search.blade.php
@@ -34,13 +34,37 @@
                             </svg>
                         </button>
                     </div>
-
                 </form>
-
             </div>
             
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
-                @include('event.partials.list') 
+                @if ($events->isEmpty())
+                <p>{{ __('No events found.') }}</p>
+                @else
+                <div id="card-layout-wrap">
+                    @foreach ($events as $event)
+                    <section class="card-list">
+                        <a class="card-link" href="#">
+                            <ul>
+                                <li>
+                                    <a href="{{ route('event.show', $event->id) }}">
+                                        <h4 class="card-title">{{ $event->name }}</h4>
+                                        <figure class="card-figure">
+                                            <img class="event_image mx-auto" src="{{ Storage::url($event->image_path) }}" alt="">
+                                        </figure>
+                                        <p class="card-text-tax">
+                                        {{ Carbon\Carbon::parse($event->date)->format('Y/m/d') }}
+                                        {{ $event->detail }}</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </a>
+                    </section>
+                    @endforeach
+                </div>
+                @endif
+                <br />
+                {{ $events->appends(request()->query())->links()}}
             </div>
         </div>
     </div>
