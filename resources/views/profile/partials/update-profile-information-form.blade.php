65e0d47382537d71731a12fd19fd8662ab1d5cc6
daiwasan <t.daiwa0219@gmail.com>
2023-10-10 16:06:39 +0900

    こんどこそ

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -27,9 +27,10 @@
 
         <div class="form-group">
             <label for="self_introduction">自己紹介文:</label>
-            <x-textarea name="self_introduction" id="self_introduction"  :value="old('self_introduction',$user->self_introduction)" required autofocus autocomplete="self_introduction" rows="4">{{ $user->self_introduction }} 
+            <x-textarea name="self_introduction" id="self_introduction"  required autocomplete="self_introduction" rows="4"> {{old('self_introduction','')}} 
             </x-textarea>
             <x-input-error class="mt-2" :messages="$errors->get('self_introduction')"/>
+                
         </div>
         <script>
             // 自己紹介文のテキストエリアを取得
