486d8146049a0ffe5fc1aaf7f25a23e077b2aa78
ENDO Takumi <endo.takumi.info@gmail.com>
2023-10-11 13:23:43 +0900

    bugfix: 入力した内容が表示されないバグを修正

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -27,24 +27,27 @@
 
         <div class="form-group">
             <label for="self_introduction">自己紹介文:</label>
-            <x-textarea name="self_introduction" id="self_introduction"  required autocomplete="self_introduction" rows="4"> {{old('self_introduction','')}} 
+
+            <x-textarea name="self_introduction" id="self_introduction" required rows="4">
+                {{ old('self_introduction', $user->self_introduction) }}
             </x-textarea>
-            <x-input-error class="mt-2" :messages="$errors->get('self_introduction')"/>
-                
+
+            <x-input-error class="mt-2" :messages="$errors->get('self_introduction')" />
+
         </div>
         <script>
             // 自己紹介文のテキストエリアを取得
             var selfIntroductionTextarea = document.getElementById('self_introduction');
-        
+
             // テキストエリアの高さを設定する関数
             function resizeTextarea() {
                 selfIntroductionTextarea.style.height = 'auto';
                 selfIntroductionTextarea.style.height = (selfIntroductionTextarea.scrollHeight) + 'px';
             }
-        
+
             // ページ読み込み時に一度高さを調整する
             window.addEventListener('load', resizeTextarea);
-        
+
             // テキストが入力されたら高さを調整する
             selfIntroductionTextarea.addEventListener('input', resizeTextarea);
         </script>
@@ -56,7 +59,8 @@
 
         <div>
             <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)" required autofocus autocomplete="name"/>
+            <x-text-input id="name" name="name" type="text" class="mt-1 block w-full" :value="old('name', $user->name)"
+                required autofocus autocomplete="name" />
         </div>
 
         <div>
